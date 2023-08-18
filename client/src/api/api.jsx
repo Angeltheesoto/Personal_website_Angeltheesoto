947c97372d77ad5f7c6c3128e45c5aed56bd37a4
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2023-08-18 16:52:28 -0400

    Fixed api url in api folder

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,10 +1,10 @@
 // get data from the back-end
-import axios from "axios";
+// import axios from "axios";
 
 export default async function getProjectsApi() {
   try {
     const API_URL =
-      "https://personal-website-angeltheesoto.vercel.app/projects";
+      "https://personal-website-angeltheesoto.vercel.app/api/projects";
     const resp = await fetch(API_URL);
     const data = await resp.json();
     return data;
